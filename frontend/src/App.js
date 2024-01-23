f6558c204a61c446070912ba193d5d9c912a5958
abdullah-devsspace <abdullah.khan@devsspace.com>
2024-01-23 23:44:56 +0500

    store starter file added using redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux
+import { Provider } from 'react-redux';
+import {store} from './store';
 
 const App = () => {
   return(
---
+++ frontend/src/store.js
@@ -0,0 +1,19 @@
+import {applyMiddleware} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from '@redux-devtools/extension'
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: composeWithDevTools(
+    applyMiddleware(...middleware)
+  ),
+  preloadedState: initialState,
+});
+
+export default store;
